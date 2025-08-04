95d20d9cc817bfa3b500ef65e5a2483bd750cf3d
Mira Khreis <s0587850@pluto7.f4.htw-berlin.de>
2025-08-04 16:52:52 +0200

    first comment

---
+++ boot.py
@@ -0,0 +1,37 @@
+import time
+from simple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'Rechnernetze'
+password = 'rnFIW625'
+mqtt_server = 'broker.f4.htw-berlin.de'
+mqtt_user = 'REPLACE_WITH_YOUR_MQTT_USERNAME'
+mqtt_pass = 'REPLACE_WITH_YOUR_MQTT_PASSWORD'
+
+#EXAMPLE IP ADDRESS
+#mqtt_server = '192.168.1.144'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'bis2025/notification'
+topic_pub = b'bis2025/fiw'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
