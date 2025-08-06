8c1966f98439f4e97d4da497a6486c0526195cf8
Mira Khreis <s0587850@pluto7.f4.htw-berlin.de>
2025-08-06 13:36:43 +0200

    Add comments to improve code readability

--- boot.py
+++ boot.py
@@ -9,22 +9,25 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
+# WLAN-Zugangdaten
 ssid = 'Rechnernetze'
 password = 'rnFIW625'
+# Konfiguration des MQTT-Brokers
 mqtt_server = 'broker.f4.htw-berlin.de'
 mqtt_user = 'REPLACE_WITH_YOUR_MQTT_USERNAME'
 mqtt_pass = 'REPLACE_WITH_YOUR_MQTT_PASSWORD'
 
-#EXAMPLE IP ADDRESS
-#mqtt_server = '192.168.1.144'
+# EXAMPLE IP ADDRESS
+# mqtt_server = '192.168.1.144'
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_sub = b'bis2025/notification'
-topic_pub = b'bis2025/gruppe5/temperature'
+topic_pub = b'bis2025/gruppe5/temperature' # Passendes Topic zum Senden
 
 last_message = 0
-message_interval = 5
+message_interval = 5 # Messintervall kleiner 5s
 counter = 0
 
+# WLAN-Verbindung aufbauen
 station = network.WLAN(network.STA_IF)
 
 station.active(True)
@@ -33,5 +36,6 @@ station.connect(ssid, password)
 while station.isconnected() == False:
   pass
 
+# Verbindungsdaten ausgeben (Debug)
 print('Connection successful')
 print(station.ifconfig())
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import machine
 import dht
 from machine import Pin
 
-
+# Empfang von MQTT-Nachrichten ermöglichen (Callback)
 def sub_cb(topic, msg):
   print((topic, msg))
   if topic == b'notification' and msg == b'received':
     print('ESP received hello message')
 
+# Verbindung zum MQTT-Broker herstellen
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub
   client = MQTTClient(client_id, mqtt_server, user=mqtt_user, password=mqtt_pass)
@@ -19,33 +20,36 @@ def connect_and_subscribe():
   client.subscribe(topic_sub)
   print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
   return client
-
+# Bei Verbindungsfehler: Neustart
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
   time.sleep(10)
   machine.reset()
 
-
+# Sensor initalisieren (DHT22 an Pin D4)
 DHT_PIN = 4
 sensor = dht.DHT22(Pin(DHT_PIN))
 
-
+# Verbindung zu MQTT aufbauen
 try:
   client = connect_and_subscribe()
 except OSError as e:
   restart_and_reconnect()
-
+# Hauptschleife für Messung, Formatierung und senden
 while True:
     try:
-        client.check_msg()
+        client.check_msg() # Auf eingehende MQTT-Nachrichten prüfen
         if (time.time() - last_message) > message_interval:
             sensor.measure()
             temperature = sensor.temperature()
-            payload = json.dumps({
+            # Daten in JSON-Format mit Zeitstampel umwandeln
+            payload = json.dumps({  
                 "temperature": temperature,
                 "timestamp": time.time()
             })
+            #Daten an MQTT-Topic senden
             client.publish(topic_pub, payload)
+            #Debug Ausgabe
             print("Gesendet:", payload)
             last_message = time.time()
             counter += 1
